35769c73566e14b3c0668746024f239ebfc9ccd3
Michael Tuhaimer <michaeltuhaimer@Michaels-MacBook-Air.local>
2024-09-16 13:39:12 -0500

    Add Favorite validation

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :pokemon
+
+  validates :pokemon_id, presence: true, uniqueness: { scope: :user_id, message: "Already favorited." }
 end
